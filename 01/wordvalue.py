883993dc8c18e5c7494ed8c058802a8e60767165
amandavinci <amanthevinci@gmail.com>
2019-11-30 11:45:01 +0100

    pythonic and optimized implementation

--- 01/wordvalue.py
+++ 01/wordvalue.py
@@ -2,7 +2,8 @@ from data import DICTIONARY, LETTER_SCORES
 
 def load_words():
     """Load dictionary into a list and return list"""
-    return [word.strip() for word in open(DICTIONARY, 'r').readlines()]
+    with open(DICTIONARY, 'r') as f:
+        return [word.strip() for word in f.readlines()]
 
 def calc_word_value(word):
     """Calculate the value of the word entered into function
@@ -12,9 +13,9 @@ def calc_word_value(word):
 def max_word_value(word_list=None):
     """Calculate the word with the max value, can receive a list
     of words as arg, if none provided uses default DICTIONARY"""
-    word_list = load_words() if word_list is None else word_list
-    word_score = {word:calc_word_value(word) for word in word_list}
-    return max(word_score, key= lambda k: word_score[k])
+    if word_list is None:
+        word_list = load_words()
+    return max(word_list, key=calc_word_value)
 
 if __name__ == "__main__":
     pass

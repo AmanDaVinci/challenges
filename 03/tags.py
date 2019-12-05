dbc2a4dd173b6c32bfab7509e133012ebae08b70
amandavinci <amanthevinci@gmail.com>
2019-12-05 13:39:46 +0100

    word similarity solved; fix word ordering

--- 03/tags.py
+++ 03/tags.py
@@ -16,13 +16,14 @@ def get_tags():
     Hint: use TAG_HTML.findall"""
     with open(RSS_FEED, 'r') as f:
         rss = f.read()
-    tags = [raw_tag.replace('_', ' ') for raw_tag in TAG_HTML.findall(rss)]
-    return tags
+    return [raw_tag.replace('-', ' ').lower()
+            for raw_tag in TAG_HTML.findall(rss)]
 
 def get_top_tags(tags):
     """Get the TOP_NUMBER of most common tags
     Hint: use most_common method of Counter (already imported)"""
-    pass
+    tag_counts = Counter(tags)
+    return tag_counts.most_common(10)
 
 
 def get_similarities(tags):
@@ -30,15 +31,20 @@ def get_similarities(tags):
     Hint 1: compare each tag, use for in for, or product from itertools (already imported)
     Hint 2: use SequenceMatcher (imported) to calculate the similarity ratio
     Bonus: for performance gain compare the first char of each tag in pair and continue if not the same"""
-    pass
-
+    similar_pairs = []
+    for pair in  product(tags, tags):
+        ratio = SequenceMatcher(None, *pair).ratio()
+        if ratio>SIMILAR and ratio!=1:
+            if (pair[1],pair[0]) not in similar_pairs:
+                similar_pairs.append(pair)
+    return similar_pairs
 
 if __name__ == "__main__":
     tags = get_tags()
     top_tags = get_top_tags(tags)
-    print('* Top {} tags:'.format(TOP_NUMBER))
+    print(f"* Top {TOP_NUMBER} tags:")
     for tag, count in top_tags:
-        print('{:<20} {}'.format(tag, count))
+        print(f"{tag:<20} {count}")
     similar_tags = dict(get_similarities(tags))
     print()
     print('* Similar tags:')

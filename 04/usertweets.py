3b5478a1adbef6cb5dfd9a6e072b3ee4eca3694a
amandavinci <amanthevinci@gmail.com>
2019-12-09 10:46:34 +0100

    implemented usertweet class; need to fix tests

--- 04/usertweets.py
+++ 04/usertweets.py
@@ -22,7 +22,11 @@ class UserTweets(object):
         to create api interface.
         Use _get_tweets() helper to get a list of tweets.
         Save the tweets as data/<handle>.csv"""
-        # ...
+        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+        api = tweepy.API(auth)
+        import pdb; pdb.set_trace()
+        self.user = api.get_user(handle, max_id=max_id)
         self._tweets = list(self._get_tweets())
         self._save_tweets()
 
@@ -31,27 +35,34 @@ class UserTweets(object):
         See tweepy API reference: http://docs.tweepy.org/en/v3.5.0/api.html
         Use a list comprehension / generator to filter out fields
         id_str created_at text (optionally use namedtuple)"""
-        pass
+        for tweet in self.user.timeline():
+            yield Tweet(id_str=tweet.id_str,
+                        created_at=tweet.created_at,
+                        text=tweet.text)
 
     def _save_tweets(self):
         """Use the csv module (csv.writer) to write out the tweets.
         If you use a namedtuple get the column names with Tweet._fields.
         Otherwise define them as: id_str created_at text
         You can use writerow for the header, writerows for the rows"""
-        pass
+        filename = f"data/{self.user.name}.csv"
+        with open(filename, 'w') as f:
+            tweetwriter = csv.writer(f, quoting=csv.QUOTE_MINIMAL)
+            tweetwriter.writerow(Tweet._fields)
+            tweetwriter.writerows(self._tweets)
 
     def __len__(self):
         """See http://pybit.es/python-data-model.html"""
-        pass
+        return len(self._tweets)
 
     def __getitem__(self, pos):
         """See http://pybit.es/python-data-model.html"""
-        pass
+        return self._tweets[pos]
 
 
 if __name__ == "__main__":
 
-    for handle in ('pybites', '_juliansequeira', 'bbelderbos'):
+    for handle in ('pybites', 'bbelderbos'):
         print('--- {} ---'.format(handle))
         user = UserTweets(handle)
         for tw in user[:5]:
